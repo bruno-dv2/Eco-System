cccdcd015fe03f682fd639fcddfde9a4f9101b33
EmanuelCosta23 <e.costa2356@gmail.com>
2025-09-19 20:11:04 -0300

    Tela de Login

--- EcoSystemMobile/App.tsx
+++ EcoSystemMobile/App.tsx
@@ -1,35 +1,13 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/screens/login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Eco System Mobile</Text>
-      <Text style={styles.subtitle}>Projeto pronto para desenvolvimento!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Login />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F8FAFC',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#2248c5ff',
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#64748B',
-    textAlign: 'center',
-  },
+  
 });
